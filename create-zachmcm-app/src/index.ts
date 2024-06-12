482b2dc6facf56f484d791f0e99c7dde7cf02ad3
Zach McMullen <zachmcmullen04@gmail.com>
2024-06-12 14:05:59 -0700

    body parser and multer

--- create-zachmcm-app/src/index.ts
+++ create-zachmcm-app/src/index.ts
@@ -1,15 +1,16 @@
 import "dotenv/config"
 import express from "express"
 import cors from "cors"
-import bodyParser from "body-parser";
-
+import multer from "multer"
 const port = process.env.PORT || 8000
 
 const app = express()
 app.use(cors())
-app.use(bodyParser.json({ limit: "200mb" }));
-app.use(bodyParser.urlencoded({ extended: true, limit: "200mb" }));
-app.use(bodyParser.text({ limit: "200mb" }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const upload = multer()
+app.use(upload.any())
 
 app.get("/", (req, res) => {
   res.json({
